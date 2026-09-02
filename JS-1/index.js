8ebe4df81b356d004654252dda78ac75b6865da4
karimnuriev25-design <karimnuriev25@gmail.com>
2026-09-02 22:38:06 +0400

    Add files via upload

---
+++ JS-1/index.js
@@ -0,0 +1,12 @@
+const firstName = "Karim";
+const lastName = "Nyriev";
+const isStudent = (true);
+const age = 40;
+const currentYear = 2026;
+
+const str = `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent} `
+
+
+console.log(firstName,lastName,isStudent,age,str)
+
+//string//
